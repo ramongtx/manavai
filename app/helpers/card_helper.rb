edca7b86f6dd33c3e0d1002f3be77edc6e145161
Ramon Carvalho Maciel <ramongtx@gmail.com>
2018-04-01 09:26:28 -0300

    Mana cost helper

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -3,4 +3,17 @@ module CardHelper
     types = card.type.split(' ').map(&:downcase) & %w[land creature instant sorcery planeswalker enchantment artifact]
     types.map { |type| "ms-#{type}" }
   end
+
+  def card_costs_icon(card)
+    return [] if card.mana_cost.blank?
+    card.mana_cost.scan(/\{([^\{\}]+)\}/).map do |cost|
+      cost = cost.first.downcase
+      cost.sub!('/', '')
+      if cost.length == 2 && !cost.include?('p')
+        "ms-cost ms-shadow ms-split ms-#{cost.downcase}"
+      else
+        "ms-cost ms-shadow ms-#{cost.downcase}"
+      end
+    end
+  end
 end
