4982890968c33b1cda5b6d53abd117bac925fd7a
Ramon Carvalho Maciel <ramongtx@gmail.com>
2018-04-14 20:52:18 -0300

    Sendgrid configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    enable_starttls_auto: true,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: 'login',
+    domain: 'manavai.com.br'
+  }
 end
